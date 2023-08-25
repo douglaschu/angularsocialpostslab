5f8c679c2ef25d23bd414937d2d6351aaa429253
Doug Chu <douglas.l.chu@gmail.com>
2023-08-25 17:15:27 -0400

    cleaned up margins

--- src/app/social-posts/social-posts.component.spec.ts
+++ src/app/social-posts/social-posts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SocialPostsComponent } from './social-posts.component';
 
@@ -6,7 +6,7 @@ describe('SocialPostsComponent', () => {
   let component: SocialPostsComponent;
   let fixture: ComponentFixture<SocialPostsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SocialPostsComponent ]
     })
